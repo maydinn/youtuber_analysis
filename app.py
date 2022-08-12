dd946dd22023fd0b121b5bcd661b7e45c226865a
maydinn <muhammedaydin9214@gmail.com>
2022-08-12 20:45:42 +0200

    'on'

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestRegressor
 import plotly.express as px
+import os
+
 
 
 
@@ -53,11 +55,12 @@ def main():
         
         
 
-        
-
-        options = webdriver.chrome.options.Options()
-        options.headless = True
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
         
 
