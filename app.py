226b21af135c1fdf1e3d5d5d62ab29dfa495eb42
maydinn <muhammedaydin9214@gmail.com>
2022-08-16 21:25:23 +0200

    ''

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestRegressor
 import plotly.express as px
 import os
-#nltk.download('punkt') 
+
 
 
 
@@ -31,91 +31,7 @@ def cleaning(data):
     #joining
     return " ".join(tokens_without_punc)
 
-
-
-
-
-def main():
-    #st.write(st.__version__)
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">enter link</h2>
-    </div><br><br>"""
-    
-    
-    
-     
-    link = st.text_input('link', placeholder = 'https://www.youtube.com/c/<channelname>/videos', key = '1')
-    
-    sentence = st.text_input('title for upcoming video', placeholder = 'Trends App in 2022', key = '2')
-    
-    if st.button('Submit'):
-     
-        
-        
-        if link.endswith( '/videos'):
-
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
-            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-
-
-            driver.get(link)
-            time.sleep(2)
-
-            driver.find_element_by_xpath("//button[@aria-label = 'Reject all']").click()
-            time.sleep(3)
-
-
-            while True:
-                scroll_height = 12000
-                document_height_before = driver.execute_script("return document.documentElement.scrollHeight")
-                driver.execute_script(f"window.scrollTo(0, {document_height_before + scroll_height});")
-                time.sleep(3)
-                document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
-                if document_height_after == document_height_before:
-                    break
-
-
-            element = WebDriverWait(driver, 30).until(
-                            EC.presence_of_all_elements_located((By.XPATH, "//*[@id='meta']"))
-                            )
-
-
-
-
-            text = []
-            view = []
-            date = []
-            st.write(element[0].text)
-            for i in element:
-
-                a = i.text.split('\n')
-
-                if (len(a) == 3) and ('waiting' not in (i.text)):
-                    for j in a:
-                        if 'CC' != j:
-                            if ' views' in j:
-                                view.append(j)
-
-                            elif ' ago' in j:
-                                if 'Streamed' in j:
-                                    date.append(' '.join(j.split()[1:]))
-                                else:
-                                    date.append(j)                   
-
-                            else:
-                                text.append(j)
-
-
-
-            df=pd.DataFrame(zip(text, view, date), columns=['text','views','date'])
-            driver.close()
-            def views(x):
+def views(x):
                 a = x.split(' ')[0]
 
                 try:
@@ -143,47 +59,131 @@ def main():
                             a = '################'
 
                 return int(a)
-
-
-            df.views = df.views.apply(views)
-
-            df['years_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'year' in x else 0 )
-            df['months_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'month' in x else 0 if 'day' in x or 'week' in x or 'minute' in x else -1 )
-            df['weeks_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'week' in x else 1 if ('day' in x and int(x.split(' ')[0]) > 6) else 0 if 'day' in x or 'minute' in x else -1 )
-            df['days_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'day' in x else 0 if 'minute' in x else -1 )
-
-
-            df_y = df[['views', 'years_ago']].groupby('years_ago').mean().reset_index()
-            df_y.years_ago = df_y.years_ago.astype(str)
-            df_m = df[df.months_ago != -1][['views', 'months_ago']].groupby('months_ago').mean().reset_index()
-            df_m.months_ago = df_m.months_ago.astype(str)
-            df_w = df[df.weeks_ago != -1][['views', 'weeks_ago']].groupby('weeks_ago').mean().reset_index()
-            df_w.weeks_ago = df_w.weeks_ago.astype(str)
-            df_d = df[df.days_ago != -1][['views', 'days_ago']].groupby('days_ago').mean().reset_index()
-            df_d.days_ago = df_d.days_ago.astype(str)
-
-            df["text2"] = df.text.apply(cleaning)
-
-            X = df['text2']
-            y = df['views']
-
-            tf_idf_word_vectorizer =TfidfVectorizer(max_df=0.95, min_df=3)
-            tf_idf_word_vectorizer.fit(X)
-
-            X_tf_idf_word = tf_idf_word_vectorizer.transform(X)
-            model = RandomForestRegressor()
-            model.fit(X_tf_idf_word, y)
-
-            feature_imp = feature_imp= pd.DataFrame(zip(model.feature_importances_,
-                           tf_idf_word_vectorizer.get_feature_names_out()), columns=['value','word']).sort_values('value',ascending=False).head(20)
+            
+            
+                
 
 
 
-            fig_y = px.bar(df_y, x='years_ago', y='views',height=500)
-            fig_m = px.bar(df_m, x='months_ago', y='views',height=500)
-            fig_w = px.bar(df_w, x='weeks_ago', y='views',height=500)
-            fig_d = px.bar(df_d, x='days_ago', y='views',height=500)
-            fig_wo = px.bar(feature_imp.sort_values('value'), x='value', y= 'word', orientation='h'  )
+
+def main():
+    
+   
+    st.title("A Snapshot of Given Youtube Channel ")
+    st.markdown("This application is a Streamlit dashboard used "
+                "to provide a brief shapshot of the a given channel 🐦")
+    
+    
+     
+    link = st.text_input('link', placeholder = 'https://www.youtube.com/c/<channelname>/videos', key = '1')
+    
+    sentence = st.text_input('title for upcoming video', placeholder = 'Trends App in 2022', key = '2')
+    
+    if st.button('Submit'):
+        with st.spinner('Wait for it...'):
+            time.sleep(2)
+        
+        
+        if link.endswith( '/videos') and link.startswith('https://www.youtube.com/c/'):
+            with st.spinner('Getting data...'):
+                chrome_options = webdriver.ChromeOptions()
+                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+                chrome_options.add_argument("--headless")
+                chrome_options.add_argument("--disable-dev-shm-usage")
+                chrome_options.add_argument("--no-sandbox")
+                driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
+
+                driver.get(link)
+                time.sleep(2)
+
+                driver.find_element_by_xpath("//button[@aria-label = 'Reject all']").click()
+                time.sleep(3)
+
+
+                while True:
+                    scroll_height = 12000
+                    document_height_before = driver.execute_script("return document.documentElement.scrollHeight")
+                    driver.execute_script(f"window.scrollTo(0, {document_height_before + scroll_height});")
+                    time.sleep(3)
+                    document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
+                    if document_height_after == document_height_before:
+                        break
+
+
+                element = WebDriverWait(driver, 30).until(
+                                EC.presence_of_all_elements_located((By.XPATH, "//*[@id='meta']"))
+                                )
+
+
+
+
+                text = []
+                view = []
+                date = []
+                st.write(element[0].text)
+                for i in element:
+
+                    a = i.text.split('\n')
+
+                    if (len(a) == 3) and ('waiting' not in (i.text)):
+                        for j in a:
+                            if 'CC' != j:
+                                if ' views' in j:
+                                    view.append(j)
+
+                                elif ' ago' in j:
+                                    if 'Streamed' in j:
+                                        date.append(' '.join(j.split()[1:]))
+                                    else:
+                                        date.append(j)                   
+
+                                else:
+                                    text.append(j)
+
+
+                driver.close()
+                df=pd.DataFrame(zip(text, view, date), columns=['text','views','date'])
+            with st.spinner('Getting data...'):
+                df.views = df.views.apply(views)
+                df['years_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'year' in x else 0 )
+                df['months_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'month' in x else 0 if 'day' in x or 'week' in x or 'minute' in x else -1 )
+                df['weeks_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'week' in x else 1 if ('day' in x and int(x.split(' ')[0]) > 6) else 0 if 'day' in x or 'minute' in x else -1 )
+                df['days_ago']=df.date.apply(lambda x: int(x.split(' ')[0]) if 'day' in x else 0 if 'minute' in x else -1 )
+            
+                #df = pd.read_csv('df.csv')
+                df_y = df[['views', 'years_ago']].groupby('years_ago').mean().reset_index()
+                df_y.years_ago = df_y.years_ago.astype(str)
+                df_m = df[df.months_ago != -1][['views', 'months_ago']].groupby('months_ago').mean().reset_index()
+                df_m.months_ago = df_m.months_ago.astype(str)
+                df_w = df[df.weeks_ago != -1][['views', 'weeks_ago']].groupby('weeks_ago').mean().reset_index()
+                df_w.weeks_ago = df_w.weeks_ago.astype(str)
+                df_d = df[df.days_ago != -1][['views', 'days_ago']].groupby('days_ago').mean().reset_index()
+                df_d.days_ago = df_d.days_ago.astype(str)
+
+                df["text2"] = df.text.apply(cleaning)
+
+                X = df['text2']
+                y = df['views']
+
+                tf_idf_word_vectorizer =TfidfVectorizer(max_df=0.95, min_df=3)
+                tf_idf_word_vectorizer.fit(X)
+
+                X_tf_idf_word = tf_idf_word_vectorizer.transform(X)
+                model = RandomForestRegressor()
+                model.fit(X_tf_idf_word, y)
+
+                feature_imp = feature_imp= pd.DataFrame(zip(model.feature_importances_,
+                               tf_idf_word_vectorizer.get_feature_names_out()), columns=['value','word']).sort_values('value',ascending=False).head(20)
+
+
+
+                fig_y = px.bar(df_y, x='years_ago', y='views',height=500)
+                fig_m = px.bar(df_m, x='months_ago', y='views',height=500)
+                fig_w = px.bar(df_w, x='weeks_ago', y='views',height=500)
+                fig_d = px.bar(df_d, x='days_ago', y='views',height=500)
+                fig_wo = px.bar(feature_imp.sort_values('value'), x='value', y= 'word', orientation='h'  )
 
 
 
@@ -225,8 +225,14 @@ def main():
                 
                 
         else:
-            st.write("""please enter the right version of link as following;
-            https://www.youtube.com/c/<channelname>/videos""")
+            with st.spinner('Wait for it...'):
+                time.sleep(2)
+            
+#             'st.write("please enter the right version of link as following;")
+#             st.write()'
+           
+            st.error("""please enter the right version of link as in link box""")
+            st.snow()
 
             
             
